1ac127595e8fb727341ea67256da53d2dacfca8e
Matheus Xavier <mxavier@bpmx.io>
2020-03-29 18:01:34 -0300

    minor fix

--- rvm.h
+++ rvm.h
@@ -1,18 +1,18 @@
 #ifndef RVM_H
-#define RVM_H
 #include <stdio.h>
 #include <jemalloc/jemalloc.h>
 #include <inttypes.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <mruby.h>
-
 typedef enum { BigEndian, LittleEndian } endianess_t;
 
 char *rvm_search_path;
 
 endianess_t rvm_det_endianess();
+
 uint64_t rvm_ingest_bytes(uint8_t *block, uint64_t to_ingest, FILE *from);
 uint8_t *rvm_bytes_to_program(const uint8_t *block);
+#define RVM_H
 
 #endif
